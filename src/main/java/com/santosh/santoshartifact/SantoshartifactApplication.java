7d3fa776c051b3fb09a6a398d901928e0e39f4f1
Santosh-Kumar-Perumal <santosh.personalid@gmail.com>
2023-06-07 16:12:02 +0100

    Initial Commit

---
+++ src/main/java/com/santosh/santoshartifact/SantoshartifactApplication.java
@@ -0,0 +1,25 @@
+package com.santosh.santoshartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class SantoshartifactApplication {
+
+	@GetMapping("/path")
+	public String welcome() {
+		return "Welcome Santosh Kumar";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantoshartifactApplication.class, args);
+	}
+
+}
+
+
+
